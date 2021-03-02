fd716a9735e4de871f0f30590de3672bfd7b6ac5
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-03-02 20:34:36 +0100

    feat(home): Get / append photographer and tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,66 +11,47 @@ fetch('/data/json/FishEyeDataFR.json')
   })
   .then(function (data) {
     console.log(data);
-    appendData(data);
+    appendPhotographerDiv(data);
   })
   .catch(function (err) {
     console.log(err);
 });
 
-/*
-var tags = ["portrait", "événements", "voyage", "animaux"];
-console.log(tags.length);
 
-function getTags() {
-    var cardContainer= document.getElementById("cardContainer");
-    for (var i = 0; i < tags.length; i++) {
-        var div = document.createElement("div");
-        div.innerHTML = (tags[i]);
-        cardContainer.appendChild(div);
-    }
+//Ajoute la liste des tags associé à la div du photographe selon son id
+function getTagsByPhotographerId(photographer) {
+    let photographerTagsContainer = $(`div#photographer-${photographer.id} > .photographer__tag`);
+    photographer.tags.forEach(tag => {
+        photographerTagsContainer.append(`
+            <li>
+                <a href="" title="Tag ${tag}" aria-label="Tag ${tag}">
+                <span aria-hidden="false">#${tag}</span></a>
+            </li>
+        `);
+    });
 }
-getTags();
-<div id="cardContainer"></div>
- http://jsfiddle.net/fx9cabqd/ */
 
-
-function appendData(data) {
+//Parcours la liste des photographes, et créer une div associé
+//Appel ensuite getTagsByPhotographerId
+function appendPhotographerDiv(data) {
     let mainContainer = $(".container__main");
-    let photographers = data.photographes;
-    for (let i = 0; i < photographers.length; i++) {
-        let photographerTags = photographers[i].tags;
+    data.photographes.forEach(photographer => {
         mainContainer.append(`
-            <div class="photographer">
+            <div class="photographer" id='photographer-${photographer.id}'>
                 <div class="photographer__img">
                     <a href="" class="photographer__img__link">
-                        <img src="./assets/img/photo-video/Photographers_ID_Photos/${photographers[i].portrait}" alt="${photographers[i].nom}">
-                        <h2>${photographers[i].nom}</h2>
+                        <img src="./assets/img/photo-video/Photographers_ID_Photos/${photographer.portrait}" alt="${photographer.nom}">
+                        <h2>${photographer.nom}</h2>
                     </a>
                 </div>
                 <div class="photographer__text">
-                    <p class="photographer__text__localisation">${photographers[i].ville}, ${photographers[i].country || photographers[i].pays}</p>
-                    <p class="photographer__text__desc">${photographers[i].tagline}</p>
-                    <p class="photographer__text__price">${photographers[i].prix} /jour</p>
+                    <p class="photographer__text__localisation">${photographer.ville}, ${photographer.country || photographer.pays}</p>
+                    <p class="photographer__text__desc">${photographer.tagline}</p>
+                    <p class="photographer__text__price">${photographer.prix} /jour</p>
                 </div>
-                <ul class="photographer__tag">
-                    <li>
-                        <a href="" title="Tag portrait" aria-label="Tag Photographer portrait">
-                        <span aria-hidden="false">#portrait</span></a>
-                    </li>
-                    <li>
-                        <a href="" title="Tag events" aria-label="Tag Photographer events">
-                        <span aria-hidden="false">#events</span></a>
-                    </li>
-                    <li>
-                        <a href="" title="Tag travel" aria-label="Tag Photographer travel">
-                        <span aria-hidden="false">#travel</span></a>
-                    </li>
-                    <li>
-                        <a href="" title="Tag animals" aria-label="Tag Photographer animals">
-                        <span aria-hidden="false">#animals</span></a>
-                    </li>
-                </ul>
+                <ul class="photographer__tag"></ul>
             </div>
         `);
-    }
+        getTagsByPhotographerId(photographer);
+    });
 }
